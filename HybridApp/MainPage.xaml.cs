f67baecdc6e2f4f2ffd77133300131aa9b39a5d2
Devlin <webmasterdevlin@gmail.com>
2022-02-02 22:28:55 +0100

    Add project files.

---
+++ HybridApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace HybridApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    async void OnButtonClicked(object sender, EventArgs args)
+    {
+        await DisplayAlert("Alert", "You have been alerted using MAUI and Blazor", "OK");
+    }
+}
